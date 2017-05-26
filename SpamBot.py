e3a33b4ffb25f23dec46de2ffda487caaa227e62
cgoeller919 <guitarguy919@gmail.com>
2017-05-26 17:23:34 -0400

    FIX For User Phrase Not Working
    Mouse Position was being grabbed at beginning, needed to happen when it detects mouse clicks so that it didn't get mouse position from when script was run. Also cleanup

--- SpamBot.py
+++ SpamBot.py
@@ -7,7 +7,8 @@ import win32api, win32com
 SpamText = ['Nerd', 'I Hate You', 'The Revenge is Sweet', 'Die in a Fire', 'Is this annoying yet?', 'Hi', 'TicTacs', 'I Like Turtles', 'Jesus is pretty Good', 'Supercalifragilisticexpialidocious', 'NERF THIS', 'HEROES NEVER DIE', 'RYUU GA WAGA TEKI WO KURAU', 'Deleted', 'Scrub', 'ITS HIGH NOON', 'OH LETS BREAK IT DOWN','FREEZE! DONT MOVE.', 'JUSTICE RAINS FROM ABOVE', 'LOL', 'You seem nice. Its a shame that I have to kill you.', 'NRF DIS', 'Mada Mada'
                   , 'DIE, DIE, DIE', "HAMMER DOWN!", "I'VE GOT YOU IN MY SIGHTS", "FIRE IN THE HOLE", 'CEASE YOUR RESISTANCE', 'APAGANDO LAS LUCES', 'EAT THIS', 'TELEPORTER ONLINE. I HAVE OPENED THE PATH.', 'TIMES UP', 'MOLTEN CORE', 'NO ONE CAN HIDE FROM MY SIGHTS', 'FIRE AT WILL', 'PASS INTO THE IRIS', 'CATCH PHRASE!', 'Cheers Love, the Cavalrys here!', 'Love, D.Va.', 'Its in the refrigerator.', 'Youre welcome', 'Hey Daddy-O', 'Boop', 'NERF THIS', 'NERF THIS', 'D.Va Online', 'Meka Activated!'
                   , 'Hey', 'I need healing.'] #spam phrases
-mposx, mposy = pyautogui.position()
+mposx = 0
+mposy = 0
 #print(mposx)
 #print(mposy)
 
@@ -19,7 +20,8 @@ def spam(): #main random spam function
     while mousePressed == False:
         a = win32api.GetKeyState(0x01) #Variable for Determining if there is a difference between previous and current state
         if a != state_left: #Compare Previous and Current States
-            mousePressed = True 
+            mousePressed = True
+            mposx, mposy = pyautogui.position() #grab mouse position
         time.sleep(.01) #Limit amount of times while loop runs
     count = 1
     while count <= endcount:
@@ -39,7 +41,8 @@ def singleSpam(userPhrase):#single phrase spam function
     while mousePressed == False:
         a = win32api.GetKeyState(0x01) #Variable for Determining if there is a difference between previous and current state
         if a != state_left: #Compare Previous and Current States
-            mousePressed = True 
+            mousePressed = True
+            mposx, mposy = pyautogui.position() #grab mouse position
         time.sleep(.01) #Limit amount of times while loop runs
     count = 1
     while count <= endcount:
@@ -55,7 +58,7 @@ def gather():
     sPhrase = eval(input('Enter 1 for a Single Phrase, or 2 for a random set of phrases: '))#determines if user wants to spam a single phrase or a prebuilt set of random phrases
     if sPhrase == 1:
         userPhrase = input('Please Enter the Phrase you would like to spam: ')#changes SpamText to user designated phrase
-        singleSpam()
+        singleSpam(userPhrase)
         print('User Phrase Spamming')#console output to verify which sPhrase choice is running
     elif sPhrase == 2: #runs preselected spam phrases
         spam()
